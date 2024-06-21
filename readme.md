2a3e80718588bb59595db311b3acc13c02b19fec
Octavian T. <octavian@tabuci.org>
2024-06-21 17:33:11 +0300

    fixes

--- readme.md
+++ readme.md
@@ -26,7 +26,7 @@ experimental:
   plugins:
     traefikdisolver:
       modulename: github.com/kyaxcorp/traefikdisolver
-      version: v1.0.3
+      version: v1.0.4
 ```
 
 ### Plugin configuration
--- serve_http.go
+++ serve_http.go
@@ -23,27 +23,30 @@ func (r *Disolver) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 	if trustResult.trusted {
-		if r.provider == providers.Cloudflare && req.Header.Get(cloudflare.CfVisitor) != "" {
-			var cfVisitorValue CFVisitorHeader
-			if err := json.Unmarshal([]byte(req.Header.Get(cloudflare.CfVisitor)), &cfVisitorValue); err != nil {
-				req.Header.Set(cloudflare.XCfTrusted, "danger")
-				req.Header.Del(cloudflare.CfVisitor)
-				req.Header.Del(cloudflare.ClientIPHeaderName)
-				r.next.ServeHTTP(rw, req)
-				return
+		switch r.provider {
+		case providers.Cloudflare:
+			if req.Header.Get(cloudflare.CfVisitor) != "" {
+				var cfVisitorValue CFVisitorHeader
+				if err := json.Unmarshal([]byte(req.Header.Get(cloudflare.CfVisitor)), &cfVisitorValue); err != nil {
+					req.Header.Set(cloudflare.XCfTrusted, "danger")
+					req.Header.Del(cloudflare.CfVisitor)
+					req.Header.Del(cloudflare.ClientIPHeaderName)
+					r.next.ServeHTTP(rw, req)
+					return
+				}
+				req.Header.Set(xForwardProto, cfVisitorValue.Scheme)
 			}
-			req.Header.Set(xForwardProto, cfVisitorValue.Scheme)
+		case providers.Cloudfront:
 		}
 
 		switch r.provider {
 		case providers.Cloudflare:
 			req.Header.Set(cloudflare.XCfTrusted, "yes")
 		case providers.Cloudfront:
-		default:
-			req.Header.Set(xForwardFor, req.Header.Get(r.clientIPHeaderName))
-			req.Header.Set(xRealIP, req.Header.Get(r.clientIPHeaderName))
 		}
 
+		req.Header.Set(xForwardFor, req.Header.Get(r.clientIPHeaderName))
+		req.Header.Set(xRealIP, req.Header.Get(r.clientIPHeaderName))
 	} else {
 		switch r.provider {
 		case providers.Cloudflare:
@@ -51,10 +54,8 @@ func (r *Disolver) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 			req.Header.Del(cloudflare.CfVisitor)
 			req.Header.Del(cloudflare.ClientIPHeaderName)
 		case providers.Cloudfront:
-		default:
-			req.Header.Set(xRealIP, trustResult.directIP)
 		}
-
+		req.Header.Set(xRealIP, trustResult.directIP)
 	}
 	r.next.ServeHTTP(rw, req)
 }
